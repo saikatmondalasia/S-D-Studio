3110f5c195df53879e128a299a2f4fa27ddadd2c
saikatmondalasia <saikat.mondal.202413@gmail.com>
2026-04-03 13:25:08 +0530

    Final update

---
+++ projects.js
@@ -0,0 +1,32 @@
+const projects = [
+  {
+    name: "HealthyMe",
+    category: "Health & Wellness",
+    image: "https://i.ibb.co/xK1rVJhj/Screenshot-2026-04-02-224422.png",
+    link: "https://saikatmondalasia.github.io/Healthyme/"
+  },
+  {
+    name: "SoundBazz",
+    category: "Music & Entertainment",
+    image: "https://i.ibb.co/rGJrnJ9f/Screenshot-2026-04-02-224510.png",
+    link: "https://www.soundbazz.com/"
+  },
+  {
+    name: "Siblings Cafe",
+    category: "Restaurant/Cafe",
+    image: "https://i.ibb.co/CpXQdVPk/Screenshot-2026-04-02-224545.png",
+    link: "https://saikatmondalasia.github.io/Siblings-cafe/"
+  },
+  {
+    name: "City Bite Cafe",
+    category: "Restaurant/Cafe",
+    image: "https://i.ibb.co/8L3PL4tr/Screenshot-2026-04-02-224626.png",
+    link: "https://saikatmondalasia.github.io/Citybitecafe/"
+  },
+  {
+    name: "Bequest Inn",
+    category: "Hotel",
+    image: "https://i.ibb.co/HLD4Z7TM/Screenshot-2026-04-02-224357.png",
+    link: "https://saikatmondalasia.github.io/Bequestinn/"
+  }
+];
